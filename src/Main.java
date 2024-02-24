2842500978ad390005e34659e62a3ad2b956dc2c
Ali Eren KÖSE <alierenkose@gmail.com>
2024-02-24 16:19:12 +0300

    123asd

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int number, total = 0, count = 0;
+
+
+        Scanner input = new Scanner(System.in);     // sayıyı iste
+        System.out.println("Sorgulamak istediğiniz sayıyı giriniz :");
+        number = input.nextInt();       // aldığın sayıyı sisteme kaydet
+
+        for (int i = 1; i <= number; i++) {         // 1'den istenilen sayıya kadar olan sayıları belirle
+            if (i % 3 == 0 || i % 4 == 0) {         // sadece 4 veya 3 'e bölnünen sayıları yazdır
+                System.out.println(i);
+                total += i;                         // yazılan i leri topla
+                count++;                            // kaç kere yazıldığını say
+
+            }
+        }
+        if (count > 0) {            // yazılan sayıların adetini kaydet
+            int avarage = total / count;        // toplam / adet = ortlama
+            System.out.println("Yazdırılan sayıların ortalaması : " + avarage);
         }
     }
-}
+}// Ali Eren KÖSE   24/02/2024 16:16
